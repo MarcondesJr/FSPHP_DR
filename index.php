22d0040a497d00baa6db006f645245cfd6ad1783
MarcondesJr <marcondestecinfo@gmail.com>
2023-04-28 09:14:27 -0400

    MVC - Aula 9 - Implemetação das Páginas blog, home, paginação e artigo

--- index.php
+++ index.php
@@ -20,6 +20,9 @@ $route->namespace("Source\App");
 $route->get("/", "Web:home");
 $route->get("/sobre", "Web:about");
 $route->get("/termos", "Web:terms");
+$route->get("/blog", "Web:blog");
+$route->get("/blog/page/{page}", "Web:blog");
+$route->get("/blog/{postName}", "Web:blogPost");
 
 /**
  * ERROR ROUTES
--- source/App/Web.php
+++ source/App/Web.php
@@ -3,6 +3,7 @@
 namespace Source\App;
 
 use Source\Core\Controller;
+use Source\Support\Pager;
 
 /**
  * Web controller
@@ -28,12 +29,12 @@ class Web extends Controller
             CONF_SITE_NAME . " - " . CONF_SITE_TITLE,
             CONF_SITE_DESC,
             url(),
-            url("/assets/images/share.jpg")
+            theme("/assets/images/share.jpg")
         );
 
         echo $this->view->render("home", [
             "head" => $head,
-            "video" => "wjYZcGdPHJo"
+            "video" => "4A2QyE3KdQ8"
         ]);
     }
 
@@ -47,12 +48,45 @@ class Web extends Controller
             "Descubra o " . CONF_SITE_NAME . " - " . CONF_SITE_DESC,
             CONF_SITE_DESC,
             url("/sobre"),
-            url("/assets/images/share.jpg")
+            theme("/assets/images/share.jpg")
         );
 
         echo $this->view->render("about", [
             "head" => $head,
-            "video" => "wjYZcGdPHJo"
+            "video" => "4A2QyE3KdQ8"
+        ]);
+    }
+
+    public function blog(?array $data): void
+    {
+        $head = $this->seo->render(
+            "Blog - " . CONF_SITE_NAME,
+            "Confira em nosso blog dicas e sacadas de como controlar melhor as suas contas. Vamos tomar um café?",
+            url("/blog"),
+            theme("/assets/images/share.jpg")
+        );
+
+        $pager = new Pager(url("/blog/page/"));
+        $pager->pager(100, 10, ($data['page'] ?? 1));
+
+        echo $this->view->render("blog", [
+            "head" => $head,
+            "paginator" => $pager->render()
+        ]);
+    }
+
+    public function blogPost(array $data): void
+    {
+        $postName = $data['postName'];
+        $head = $this->seo->render(
+            "POST NAME - " . CONF_SITE_NAME,
+            "POST HEADLINE",
+            url("/blog/{$postName}"),
+            theme("BLOG IMAGE")
+        );
+        echo $this->view->render("blog-post", [
+            "head" => $head,
+            "data" => $this->seo->data()
         ]);
     }
 
@@ -66,7 +100,7 @@ class Web extends Controller
             CONF_SITE_NAME . " - Termos de USO",
             CONF_SITE_DESC,
             url("/termos"),
-            url("/assets/images/share.jpg")
+            theme("/assets/images/share.jpg")
         );
 
         echo $this->view->render("terms", [
@@ -92,7 +126,7 @@ class Web extends Controller
             "{$error->code} | {$error->title}",
             $error->message,
             url_back("/ops/{$error->code}"),
-            url("/assets/images/share.jpg"),
+            theme("/assets/images/share.jpg"),
             false
         );
 
