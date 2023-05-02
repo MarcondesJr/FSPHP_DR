5b268226dd840d70d253d85cb6339164538959ea
MarcondesJr <marcondestecinfo@gmail.com>
2023-05-02 17:45:38 -0400

    MVC - Modelos e regras de negócio - Aula 06 - Blog home e página de artigo

--- source/App/Web.php
+++ source/App/Web.php
@@ -84,11 +84,13 @@ class Web extends Controller
             theme("/assets/images/share.jpg")
         );
 
-        $pager = new Pager(url("/blog/page/"));
-        $pager->pager(100, 10, ($data['page'] ?? 1));
+        $blog = (new Post())->find();
+        $pager = new Pager(url("/blog/p/"));
+        $pager->pager($blog->count(), 9, ($data['page'] ?? 1));
 
         echo $this->view->render("blog", [
             "head" => $head,
+            "blog" => $blog->limit($pager->limit())->offset($pager->offset())->fetch(true),
             "paginator" => $pager->render()
         ]);
     }
@@ -100,16 +102,27 @@ class Web extends Controller
      */
     public function blogPost(array $data): void
     {
-        $postName = $data['postName'];
+        $post = (new Post())->findByUri($data['uri']);
+        if (!$post) {
+            redirect("/404");
+        }
+        $post->views += 1;
+        $post->save();
+
         $head = $this->seo->render(
-            "POST NAME - " . CONF_SITE_NAME,
-            "POST HEADLINE",
-            url("/blog/{$postName}"),
-            theme("BLOG IMAGE")
+            "{$post->title} - " . CONF_SITE_NAME,
+            $post->subtitle,
+            url("/blog/{$post->uri}"),
+            image($post->cover, 1200, 628)
         );
         echo $this->view->render("blog-post", [
             "head" => $head,
-            "data" => $this->seo->data()
+            "post" => $post,
+            "related" => (new Post())
+                ->find("category = :c AND id != :i", "c={$post->category}&i={$post->id}")
+            ->order("rand()")
+            ->limit(3)
+            ->fetch(true)
         ]);
     }
 
--- themes/cafeweb/blog.php
+++ themes/cafeweb/blog.php
@@ -2,9 +2,9 @@
 
 <section class="blog_page">
     <header class="blog_page_header">
-        <h1>BLOG</h1>
-        <p>Confira nossas dicas para controlar melhor suas contas</p>
-        <form name="search" action="<?= url("/blog"); ?>" method="post" enctype="multipart/form-data">
+        <h1><?= ($title ?? "BLOG"); ?></h1>
+        <p><?= ($search ?? "Confira nossas dicas para controlar melhor suas contas"); ?></p>
+        <form name="search" action="<?= url("/blog/buscar"); ?>" method="post" enctype="multipart/form-data">
             <label>
                 <input type="text" name="s" placeholder="Encontre um artigo:"/>
                 <button class="icon-search icon-notext"></button>
@@ -12,26 +12,32 @@
         </form>
     </header>
 
-    <!--EMPTY CONTENT-->
-    <div class="content content">
-        <div class="empty_content">
-            <img class="empty_content_cover" title="Empty Content" alt="Empty Content"
-                 src="<?= theme("/assets/images/empty-content.jpg"); ?>"/>
-            <h3 class="empty_content_title">Ooops, não temos conteúdo aqui :/</h3>
-            <p class="empty_content_desc">Ainda estamos trabalhando, em breve teremos novidades para você :)</p>
-            <a href="<?= url("/blog"); ?>" title="Blog"
-               class="empty_content_btn gradient gradient-green gradient-hover radius">Voltar ao blog</a>
+    <?php if (empty($blog) && !empty($search)): ?>
+        <div class="content content">
+            <div class="empty_content">
+                <h3 class="empty_content_title">Sua pesquisa não retornou resultados :/</h3>
+                <p class="empty_content_desc">Você pesquisou por <b><?= "TESTE"; ?></b>. Tente outros termos.!</p>
+                <a class="empty_content_btn gradient gradient-green gradient-hover radius"
+                   href="<?= url("/blog"); ?>" title="Blog">... ou volte ao blog</a>
+            </div>
         </div>
-    </div>
-
-    <!--BLOG-->
-    <div class="blog_content container content">
-        <div class="blog_articles">
-            <?php for ($i = 0; $i <= 8; $i++): ?>
-                <?php $this->insert("blog-list"); ?>
-            <?php endfor; ?>
+    <?php elseif(empty($blog)):?>
+        <div class="content content">
+            <div class="empty_content">
+                <h3 class="empty_content_title">Ainda estamos trabalhando aqui!</h3>
+                <p class="empty_content_desc">Nossos editores estão preparando um conteúdo de primeira para você.</p>
+            </div>
         </div>
+    <?php else: ?>
+        <div class="blog_content container content">
+            <div class="blog_articles">
+                <?php foreach($blog as $post): ?>
+                    <?php $this->insert("blog-list", ["post" => $post]); ?>
+                <?php endforeach; ?>
+            </div>
 
-        <?= $paginator; ?>
-    </div>
+            <?= $paginator; ?>
+        </div>
+    <?php endif; ?>
+
 </section>
